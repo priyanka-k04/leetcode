128e6711a9ade01a388f38c16856721c9e570cd3
priyanka-k04 <priyanka190807@gmail.com>
2025-10-21 01:03:10 +0530

    Time: 6 ms (98.57%), Space: 48 MB (56.89%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs==null || strs.length==0) return new ArrayList<>();
+        Map<String,List<String>> map=new HashMap<>();
+
+        for(String str:strs){
+            char[] ch=str.toCharArray();
+            Arrays.sort(ch);
+            String sorted=new String(ch);
+            if(!map.containsKey(sorted)){
+                map.put(sorted, new ArrayList<>());
+            }
+            map.get(sorted).add(str);
+        }
+        return new ArrayList<>(map.values());
+        
+    }
+}
