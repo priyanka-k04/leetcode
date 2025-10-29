5ff4b9caaf7ba729909c9734055b1c9c67c98aa1
priyanka-k04 <priyanka190807@gmail.com>
2025-10-30 02:32:41 +0530

    Time: 0 ms (100%), Space: 40.8 MB (46.65%) - LeetHub

---
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int smallestNumber(int n) {
+         int result=1;
+        while(result<n){
+            result=2*result+1;
+        }
+        return result;
+    }
+}
