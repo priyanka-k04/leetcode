d3ace82eb7395f3511fcc21d4a5ab302f7a700e8
priyanka-k04 <priyanka190807@gmail.com>
2025-11-03 23:51:04 +0530

    Time: 28 ms (35.17%), Space: 150.8 MB (5.52%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.java
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.java
@@ -1,48 +1,25 @@
 class Solution {
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        
-        int[][] grid = new int[m][n];
-        // guard = 1, wall = -1, guarded cell = 2, empty = 0
-
-        // Mark guards
-        for (int[] guard : guards) {
-            grid[guard[0]][guard[1]] = 1;
+       int[][] grid= new int[m][n];
+       for(int[] wall:walls) grid[wall[0]][wall[1]]=1;
+       for(int[] guard:guards) grid[guard[0]][guard[1]]=2;
+       int [][] dirs={{-1,0},{1,0},{0,1},{0,-1}};
+       for(int[] guard:guards){
+        for(int[]dir:dirs){
+            dfs(grid,dir,guard[0]+dir[0],guard[1]+dir[1]);
         }
+       }
+       int count=0;
+       for(int i=0;i<m;i++)
+        for(int j=0;j<n;j++)
+        if(grid[i][j]==0) count++;
+        return count;
 
-        // Mark walls
-        for (int[] wall : walls) {
-            grid[wall[0]][wall[1]] = -1;
-        }
-
-        // Directions: down, up, right, left
-        int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-        int count = 0;
-
-        // For each guard, spread visibility
-        for (int i = 0; i < guards.length; i++) {
-            int row = guards[i][0];
-            int col = guards[i][1];
-
-            for (int[] dir : dirs) {
-                int r = row + dir[0];
-                int c = col + dir[1];
-
-                // Move in current direction until blocked
-                while (r >= 0 && r < m && c >= 0 && c < n &&
-                       grid[r][c] != 1 && grid[r][c] != -1) {
-
-                    if (grid[r][c] == 0) {
-                        count++;         // guarding this cell
-                        grid[r][c] = 2;  // mark as guarded
-                    }
-
-                    r += dir[0];
-                    c += dir[1];
-                }
-            }
-        }
-
-        // total unguarded = total cells - guarded cells - guards - walls
-        return m * n - count - guards.length - walls.length;
+    }
+    private void dfs(int[][] grid,int[] dir,int r,int c){
+        int m=grid.length, n=grid[0].length;
+        if(r<0||r>=m||c<0||c>=n||grid[r][c]==1||grid[r][c]==2) return;
+        if(grid[r][c]==0) grid[r][c]=3;
+        dfs(grid,dir,r+dir[0],c+dir[1]);
     }
 }
