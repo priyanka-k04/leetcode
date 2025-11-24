8648b3fafd828bbf38c63fac59384c47ff73db23
priyanka-k04 <priyanka190807@gmail.com>
2025-11-25 01:18:09 +0530

    Time: 0 ms (100%), Space: 42.7 MB (25.01%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,48 +1,24 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        // Step 1: Split string into words
-        String[] words = s.split(" ");
-        
-        // Step 2: Length check - pattern and words must have same length
-        if (pattern.length() != words.length) {
-            return false;
-        }
-        
-        // Step 3: Create two HashMaps for bi-directional mapping
-        HashMap<Character, String> charToWord = new HashMap<>();
-        HashMap<String, Character> wordToChar = new HashMap<>();
-        
-        // Step 4: Check each character-word pair
-        for (int i = 0; i < pattern.length(); i++) {
-            char c = pattern.charAt(i);
-            String word = words[i];
-            
-            // Check if character already has a mapping
-            if (charToWord.containsKey(c)) {
-                // If mapped to different word, return false
-                if (!charToWord.get(c).equals(word)) {
-                    return false;
-                }
-            } else {
-                // Create new mapping
-                charToWord.put(c, word);
-            }
-            
-            // Check if word already has a mapping
-            if (wordToChar.containsKey(word)) {
-                // If mapped to different character, return false
-                if (wordToChar.get(word) != c) {
-                    return false;
-                }
-            } else {
-                // Create new mapping
-                wordToChar.put(word, c);
-            }
-        }
-        
-        // All checks passed
-        return true;
+        HashMap<Character, String> mapPS = new HashMap<>();
+        HashMap<String, Character> mapSP = new HashMap<>();
 
-        
+        String[] arr = s.split(" ");
+        if(arr.length != pattern.length()) return false;
+        for (int i = 0; i < arr.length; i++) {
+            char ch = pattern.charAt(i);
+            String word = arr[i];
+            if(mapPS.containsKey(ch)){
+                if(!mapPS.get(ch).equals(word)) return false;
+            }else{
+                mapPS.put(ch,word);
+            }
+            if(mapSP.containsKey(word)){
+                if(mapSP.get(word) != ch) return false;
+            }else{
+                mapSP.put(word,ch);
+            }
+        }
+        return true;
     }
 }
