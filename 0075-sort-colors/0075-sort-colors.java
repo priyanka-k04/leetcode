8b2e572665d16eac1b73335ad0cd5b27b642afa6
priyanka-k04 <priyanka190807@gmail.com>
2026-01-07 19:49:32 +0530

    Time: 0 ms (100%), Space: 43.6 MB (24.81%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -16,7 +16,7 @@ class Solution {
             }
         }
     }
-    private void swap(int[] nums, int i, int j){
+    private void swap(int[] nums,int i,int j){
         int temp=nums[i];
         nums[i]=nums[j];
         nums[j]=temp;
