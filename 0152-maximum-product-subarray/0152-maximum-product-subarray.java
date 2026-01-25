399e41cf05dca94c399c82d068f40d7809077fbf
priyanka-k04 <priyanka190807@gmail.com>
2026-01-26 00:55:50 +0530

    Time: 1 ms (90.53%), Space: 47.7 MB (9.77%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -3,15 +3,15 @@ class Solution {
         int n=nums.length;
         int pre=1;
         int suff=1;
-        int ans=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
+        int max=Integer.MIN_VALUE;
+        for(int i =0;i<n;i++){
             if(pre==0) pre=1;
             if(suff==0) suff=1;
             pre=pre*nums[i];
             suff=suff*nums[n-i-1];
-            ans=Math.max(ans, Math.max(pre,suff));
-        }
-        return ans;
+            max=Math.max(max,Math.max(pre,suff));
 
+        }
+        return max;
     }
 }
