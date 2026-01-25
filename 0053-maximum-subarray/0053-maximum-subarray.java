4abe01c808a279e9e0ea3b47d02f1931f6630a7e
priyanka-k04 <priyanka190807@gmail.com>
2026-01-26 00:30:05 +0530

    Time: 1 ms (99.6%), Space: 77.3 MB (39.84%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,7 +1,10 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int currsum=0;
-        int maxsum=0;
+        int maxsum=Integer.MIN_VALUE;// cant use maxsum=0 because of the case when nums=[-1] its giving answer 0 which is wrong
+        /*why? - currsum = -1
+                 maxsum = max(-1, 0) = 0
+                 return 0 ❌ (wrong)*/
         for(int i:nums){
             currsum+=i;
             maxsum=Math.max(currsum, maxsum);
