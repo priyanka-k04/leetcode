3a87a54aa1933d3acde1ac3e11a8c3aff6c0b92a
priyanka-k04 <priyanka190807@gmail.com>
2026-02-02 22:56:33 +0530

    Time: 7 ms (87.9%), Space: 46.3 MB (47.63%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,33 +1,21 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        //sliding windoe + hash map
-         int[] freq = new int[26];
-    int left = 0;
-    int maxFreq = 0;
-    int maxWindow = 0;
-    
-    for (int right = 0; right < s.length(); right++) {
-        // Update the frequency of the current character
-        freq[s.charAt(right) - 'A']++;
-        
-        // Update the max frequency in current window
-        maxFreq = Math.max(maxFreq, freq[s.charAt(right) - 'A']);
-        
-        // Calculate current window length
-        int windowLength = right - left + 1;
-        
-        // If the number of characters to replace exceeds k,
-        // then we need to shrink the window
-        if (windowLength - maxFreq > k) {
-            freq[s.charAt(left) - 'A']--;
-            left++;
-        }
-        
-        // Update max window length
-        windowLength = right - left + 1;
-        maxWindow = Math.max(maxWindow, windowLength);
-    }
-        return maxWindow;
+        int[] freq = new int[26];
+        int left = 0, right = 0;
+        int maxfreq = 0, maxsubstring = 0;
 
-}
+        while (right < s.length()) {
+            freq[s.charAt(right) - 'A']++;
+            maxfreq = Math.max(maxfreq, freq[s.charAt(right) - 'A']);
+
+            while ((right - left + 1) - maxfreq > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            maxsubstring = Math.max(maxsubstring, right - left + 1);
+            right++; // ✅ VERY IMPORTANT
+        }
+        return maxsubstring;
+    }
 }
