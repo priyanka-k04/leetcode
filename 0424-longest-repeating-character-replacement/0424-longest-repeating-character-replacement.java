aa89cbc606f498ac6238bbab16bfe95033f3a74c
priyanka-k04 <priyanka190807@gmail.com>
2026-02-03 19:05:05 +0530

    Time: 6 ms (98.6%), Space: 46.2 MB (61.23%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,20 +1,22 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int[] freq = new int[26];
-        int left = 0, right = 0;
-        int maxfreq = 0, maxsubstring = 0;
-
-        while (right < s.length()) {
-            freq[s.charAt(right) - 'A']++;
-            maxfreq = Math.max(maxfreq, freq[s.charAt(right) - 'A']);
-
-            while ((right - left + 1) - maxfreq > k) {
-                freq[s.charAt(left) - 'A']--;
+        int[] freq=new int[26];
+        char[] string=s.toCharArray();
+        int maxsubstring=0;
+        int maxfreq=0;
+        int left=0;
+        int right=0;
+        while(right<s.length()){
+            freq[string[right]-'A']++;
+            maxfreq=Math.max(maxfreq,freq[string[right]-'A']);
+        int window_size=right-left+1;
+            while(window_size-maxfreq>k){
+                freq[string[left]-'A']--;
                 left++;
+                window_size=right-left+1;
             }
-
-            maxsubstring = Math.max(maxsubstring, right - left + 1);
-            right++; // ✅ VERY IMPORTANT
+        maxsubstring=Math.max(maxsubstring,right-left+1);
+        right++;
         }
         return maxsubstring;
     }
